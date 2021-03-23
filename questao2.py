58c56bb0931d1a884736b2b8b02b0ed53346e106
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-03-23 12:33:56 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,4 +1,3 @@
-
 def main(works):
     sentence = works.lower()
 
@@ -8,15 +7,12 @@ def main(works):
 
     while i <= n- 1:
 
-        # Count occurrences of 
-        # current character
         count = 1
         while (i < n - 1 and sentence[i] == sentence[i + 1]):
             count += 1
             i += 1
         i += 1
  
-        # Print character and its count
         # print(sentence[i - 1] + str(count), end = "")
         listCount.append(sentence[i - 1] + str(count))
 
@@ -31,3 +27,5 @@ if __name__ == "__main__":
 
     sentence = input()
     main(sentence)
+
+
